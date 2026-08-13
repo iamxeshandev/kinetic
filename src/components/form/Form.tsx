2c0adef316361cec514dd71774ea4024ea009aeb
Zeshan Mehmood <iamxeshan@gmail.com>
2026-08-13 10:34:18 +0500

    feat: add form components; implement Form, FormSelect, and FormTextField with react-hook-form integration

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,24 @@
+import {
+  FormProvider,
+  type FieldValues,
+  type SubmitHandler,
+  type UseFormReturn,
+} from 'react-hook-form';
+
+type FormProps<TFieldValues extends FieldValues = FieldValues> = {
+  methods: UseFormReturn<TFieldValues>;
+  onSubmit: SubmitHandler<TFieldValues>;
+  children: React.ReactNode;
+};
+
+export function Form<TFieldValues extends FieldValues = FieldValues>({
+  methods,
+  onSubmit,
+  children,
+}: FormProps<TFieldValues>) {
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+}
---
+++ src/components/form/FormSelect.tsx
@@ -0,0 +1,25 @@
+import { Select, type SelectProps } from '@mui/material';
+import {
+  Controller,
+  useFormContext,
+  type ControllerProps,
+} from 'react-hook-form';
+
+type FormSelectProps = Omit<SelectProps, keyof ControllerProps> & {
+  name: ControllerProps['name'];
+};
+
+export function FormSelect({ name, children, ...props }: FormSelectProps) {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <Select {...field} {...props}>
+          {children}
+        </Select>
+      )}
+    />
+  );
+}
