509c3649d6da2a61ce398bfd162af9d26fc42d22
iamxeshandev <iamxeshandev@users.noreply.github.com>
2026-08-20 14:16:06 +0000

    deploy: c526bf4e4c7241108b9735a5f852258751999e8c

--- assets/SignUpPage-BnhbqQeK.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./jsx-runtime-I8uHACss.js";import{n}from"./router-DsbTrQn6.js";import{t as r}from"./config-BIMMi9m_.js";var i=e(),a=t();function o(){let e=(0,i.c)(1),t;return e[0]===Symbol.for(`react.memo_cache_sentinel`)?(t=(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(`title`,{children:`Sign Up | ${r.appName}`}),(0,a.jsx)(n,{})]}),e[0]=t):t=e[0],t}export{o as Component};
