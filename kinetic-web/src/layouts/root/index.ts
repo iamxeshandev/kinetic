65fcee957f06912cc6e44125aef2a19fb9a053ac
Zeshan Mehmood <iamxeshan@gmail.com>
2026-08-24 19:58:29 +0500

    feat: Remove RootLayout, LoadingBar, and rootLoader components to streamline layout structure

--- kinetic-web/src/layouts/root/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './rootLaoder';
-export * from './RootLayout';
--- kinetic-web/src/layouts/root/rootLaoder.ts
+++
@@ -1,9 +0,0 @@
-import { authApi } from '../../features/auth';
-
-export const rootLoader = () =>
-  authApi
-    .me()
-    .then((response) => response.data)
-    .catch(() => null);
-
-export { rootLoader as loader };
