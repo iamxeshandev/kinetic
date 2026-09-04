9e118b3e49384036ba469b05989aa90c116ba944
Zeshan Mehmood <iamxeshan@gmail.com>
2026-09-04 20:42:48 +0500

    feat: add EnumExtensions and TaskExtensions for enhanced enum display and async task data retrieval

--- kinetic-api/Extensions/HttpContextAccessorExtensions.cs
+++ kinetic-api/Extensions/HttpContextAccessorExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Claims;
+﻿using System.Net;
+using System.Security.Claims;
+using kinetic_api.Exceptions;
 
 namespace kinetic_api.Extensions;
 
@@ -6,6 +8,7 @@ public static class HttpContextAccessorExtensions
 {
     public static Guid GetUserId(this IHttpContextAccessor accessor)
     {
-        return accessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)?.ToGuid() ?? Guid.Empty;
+        return accessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)?.ToGuid() ??
+               throw new ApiException(HttpStatusCode.Unauthorized, "You have been logged out.");
     }
 }
---
+++ kinetic-api/Extensions/TaskExtensions.cs
@@ -0,0 +1,19 @@
+﻿using kinetic_api.Dtos.Common;
+
+namespace kinetic_api.Extensions;
+
+public static class TaskExtensions
+{
+    public static async Task<T?> TryGetDataAsync<T>(this Task<Response<T>> task)
+    {
+        try
+        {
+            var response = await task;
+            return response.Data;
+        }
+        catch
+        {
+            return default;
+        }
+    }
+}
