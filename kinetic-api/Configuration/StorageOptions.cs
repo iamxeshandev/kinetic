f94da982b58c29c845490a1a0c2e0116dd533b9b
Zeshan Mehmood <iamxeshan@gmail.com>
2026-09-10 20:46:53 +0500

    feat: add StorageOptions class for configuration of storage paths

---
+++ kinetic-api/Configuration/StorageOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace kinetic_api.Configuration;
+
+public class StorageOptions
+{
+    public required string Root { get; set; }
+    public required string Public { get; set; }
+    public required string Private { get; set; }
+}
