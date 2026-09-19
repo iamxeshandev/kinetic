27ec61f94cf729ae9dec4205379abb868a128a82
Zeshan Mehmood <iamxeshan@gmail.com>
2026-09-19 22:48:05 +0500

    fix: update import path for useProject hook in ProjectDetailsPage component

--- kinetic-web/src/pages/project-details/ProjectDetailsPage.tsx
+++ kinetic-web/src/pages/project-details/ProjectDetailsPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router';
 import { CONFIG } from '../../config';
-import { useProject } from '../../features/projects/hooks';
+import { useProject } from '../../features/projects/hooks/use-projects';
 import { ProjectDetailsView } from '../../features/projects/ui';
 import { LoadingScreen } from '../../shared/ui';
 
