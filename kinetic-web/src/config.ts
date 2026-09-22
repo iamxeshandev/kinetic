4b06139cf7204b2f217f488e50695362febd2e43
Zeshan Mehmood <iamxeshan@gmail.com>
2026-09-22 21:58:32 +0500

    feat: add APP_URL and API_URL to configuration

--- kinetic-web/src/config.ts
+++ kinetic-web/src/config.ts
@@ -3,6 +3,9 @@ export const CONFIG = {
   APP_DESCRIPTION:
     'The all-in-one workspace for modern teams to plan, track, and collaborate in real-time.',
 
+  APP_URL: import.meta.env.VITE_BASE_URL,
+  API_URL: import.meta.env.VITE_API_BASE_URL,
+
   STORAGE_KEYS: {
     USER: 'user',
   },
